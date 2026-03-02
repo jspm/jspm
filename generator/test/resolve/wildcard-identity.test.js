c09e546390db0d5cc3b130d9969406694fd8c8bb
Guy Bedford <guybedford@gmail.com>
2026-03-01 19:59:38 -0800

    4.4.0

--- generator/test/resolve/wildcard-identity.test.js
+++ generator/test/resolve/wildcard-identity.test.js
@@ -35,8 +35,7 @@ import assert from 'assert';
 
 // With combineSubpaths: 'both', identity wildcard exports are combined
 // into a single folder mapping
-if (typeof window === 'undefined') {
-  // TODO: uncomment once import-map@1.3.0 is released
+{
   const generator = new Generator({
     mapUrl: import.meta.url,
     defaultProvider: 'nodemodules',
